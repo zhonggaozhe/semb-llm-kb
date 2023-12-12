864d0d8c1728147bcf390c7db593aad38fc7d419
lenmon <lenmon@gmail.ccom>
2023-12-12 14:59:57 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,6 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <h1 className="text-blue-500">I'm blue!</h1>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
